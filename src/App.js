0b16bf7c48c3fe1ee7ad5f312459e9ce338f0c62
ghoshrohit72 <rohitofficial95@outlook.com>
2024-02-04 00:11:19 +0530

    Context for dark and light and puttitng in App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
 
 function App() {
+  const [theme, colorMode] = useMode();
   return (
-    <div className="app">
-   
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content"></main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
